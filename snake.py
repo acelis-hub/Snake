3536aede638c5a7d5ba1c7b1fe9d8df15129214d
Alejo <acelis@unal.edu.co>
2022-05-29 10:32:06 -0500

    funciones de movimiento

--- snake.py
+++ snake.py
@@ -1,9 +1,48 @@
 import turtle
+import time
+
+posponer = 0.5 # segundos
 
 #creacion de ventana
 
 wn = turtle.Screen() # objeto ventana
 wn.title("Snake Game") # titulo 
-wn.bgcolor("black") , # fondo 
-wn.setup(width = 600, height = 600) # dimenciones
+wn.bgcolor("white") , # fondo 
+wn.setup(width = 600, height = 600) # dimenciones en pixeles
 wn.tracer(0) # algo placentero
+
+# Cabeza de serpiente
+
+cabeza = turtle.Turtle() # objeto Turtle
+cabeza.speed(0)
+cabeza.shape("square") # forma de cuadrado
+cabeza.color("red")
+cabeza.penup() # quitar rastro
+cabeza.goto(0,0) # posicion inicial
+cabeza.direction = "down"
+
+#Funciones de movimiento
+
+def mov():
+	if cabeza.direction == "up":
+		y = cabeza.ycor()
+		cabeza.sety(y + 20)
+
+	if cabeza.direction == "down":
+		y = cabeza.ycor()
+		cabeza.sety(y - 20)
+
+	if cabeza.direction == "left":
+		x = cabeza.xcor()
+		cabeza.setx(x - 20)
+
+	if cabeza.direction == "right":
+		x = cabeza.xcor()
+		cabeza.setx(x + 20)
+
+#los juegos corren en bucles
+
+while True:
+	wn.update()
+	mov()
+	time.sleep(posponer)
